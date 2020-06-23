d3ec6def59cc84c7239a6d9a5d49744e1ddc02c4
gaurav <gaurav.adhikari36@gmail.com>
2020-06-23 18:55:49 +0545

    Model Integration succesful

--- app/src/main/java/gaurav/handgesturedetector/MainActivity.java
+++ app/src/main/java/gaurav/handgesturedetector/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -20,12 +21,16 @@ import com.google.android.material.textview.MaterialTextView;
 
 import org.tensorflow.lite.Interpreter;
 
+import gaurav.handgesturedetector.Detector.ImageClassifier;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
     private static final int CAMERA_REQUEST = 1888;
     ImageView imageView;
     MaterialTextView tv_resulttext;
     MaterialButton btn_capture;
+    ImageClassifier imageClassifier;
 
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
@@ -34,17 +39,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView=findViewById(R.id.imageView_display);
-        tv_resulttext=findViewById(R.id.tv_resulttext);
-        btn_capture=findViewById(R.id.btn_capture);
+        imageClassifier = new ImageClassifier(this);
+
+        imageView = findViewById(R.id.imageView_display);
+        tv_resulttext = findViewById(R.id.tv_resulttext);
+        btn_capture = findViewById(R.id.btn_capture);
 
         btn_capture.setOnClickListener(view -> {
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-            {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-            }
-            else
-            {
+            } else {
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
@@ -54,29 +58,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION_CODE)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
-            else
-            {
+            } else {
                 Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
             }
         }
     }
 
+    public MainActivity() {
+        super();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
-        {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
+
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(photo, 28, 28, false);
+            String guess = imageClassifier.classify(scaledBitmap);
+
+            Log.d(TAG, "Found Image = " + guess);
+            tv_resulttext.setVisibility(View.VISIBLE);
+            tv_resulttext.setText(String.format("%s %s",getResources().getString(R.string.Result_txt),String.valueOf(guess)));
         }
     }
 
